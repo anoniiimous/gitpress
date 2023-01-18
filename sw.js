ea9a1670d147ecd6c61c3f5e90b5a022a32df1ad
thebanon <91961816+thebanon@users.noreply.github.com>
2023-01-17 23:37:26 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,10 @@
 self.addEventListener('fetch', function(event) {
     const request = event.request;
-    const newRequest = new Request(event.request, {
+    const settings = {
         headers: {"Authorization": "Bearer XXX-my-token"},
         mode: "cors"
-    });
+    };
+    const newRequest = new Request(event.request);
+    console.log(event.request, { settings });
     return fetch(newRequest);
 })
