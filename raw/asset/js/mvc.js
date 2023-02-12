aa84350115ff38c6dbedf8c5f8533b4679a67560
Anon ||| Mous <3849997+anoniiimous@users.noreply.github.com>
2023-02-12 15:07:34 -0500

    Update mvc.js

--- raw/asset/js/mvc.js
+++ raw/asset/js/mvc.js
@@ -218,7 +218,7 @@ window.mvc.v ? null : (window.mvc.v = view = function(route) {
                                     owner: user.login,
                                     path: "/raw/media/media.json",
                                     repo: get[1]
-                                }).then(async(d)=>{
+                                }).then(async(d)=>{ console.log(d)
                                     var data = JSON.parse(atob(d.content));
                                     if (data) {
                                         console.log(84, {
