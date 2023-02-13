b6428ea459f8ca7778075aaedc1aca7062c600ae
thebanon <91961816+thebanon@users.noreply.github.com>
2023-02-13 04:25:57 -0500

    Update mvc.js

--- raw/asset/js/mvc.js
+++ raw/asset/js/mvc.js
@@ -219,6 +219,9 @@ window.mvc.v ? null : (window.mvc.v = view = function(route) {
                                     path: "/raw/media/media.json",
                                     repo: get[1]
                                 }).then(async(d)=>{
+                                    console.log(222, {
+                                        d
+                                    });
                                     var data = JSON.parse(atob(d.content));
                                     if (data) {
                                         console.log(84, {
