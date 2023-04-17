8348a9cac1ad59cbfb557a5753f6169528b1cc6a
thebanon <91961816+thebanon@users.noreply.github.com>
2023-04-16 21:56:15 -0400

    Update router.js

--- raw/asset/js/router.js
+++ raw/asset/js/router.js
@@ -343,7 +343,7 @@ window.rout.ing = (pages,href,GOT,n)=>{
         return bool
     })
     pgs = [...new Set(pgs)];    
-    0 > 1 ? pgs.sort(function(a, b) {
+    0 < 1 ? pgs.sort(function(a, b) {
         var bool = b.endsWith('*') && rout.ed.dir(a).length === rout.ed.dir(b).length ? -1 : 1;
         console.log(347, {a, b});
         return bool;
