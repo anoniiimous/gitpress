9265dac86d815f18b0e3384ee415d644a9c73d5e
thebanon <91961816+thebanon@users.noreply.github.com>
2023-04-19 18:29:49 -0400

    Update router.js

--- raw/asset/js/router.js
+++ raw/asset/js/router.js
@@ -126,8 +126,11 @@ window.rout.e = state=>{
             var al = a.split('/').length - 1;
             var bl = b.split('/').length - 1;
             var bool = b.endsWith('*') && al === bl ? -1 : 1;
-            bool = 1;
-            0 > 1 ? console.log(347, bool, {
+            //bool = 1;
+            0 < 1 ? console.log(347, {
+                bool,
+                ew: b.endsWith('*'),                                
+            }, {
                 a,
                 b
             }, {
