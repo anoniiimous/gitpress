2ef0d6fc9fa1fb4491ed7538cac0849f7884af0f
Anon ||| Mous <3849997+anoniiimous@users.noreply.github.com>
2023-04-24 12:55:26 -0400

    Update mvc.v.js

--- raw/asset/js/mvc.v.js
+++ raw/asset/js/mvc.v.js
@@ -423,7 +423,7 @@ window.mvc.v ? null : (window.mvc.v = view = function(route) {
                                         //doc.setAttribute('class', 'border-1px-solid border-color-ddd border-radius-15pct box-shadow-0px-1px-6px-0px height-100pct position-absolute top-0 width-100pct');
                                         //console.log(svg);
                                         svg.insertAdjacentHTML('beforeend', svgstr);
-                                        svg.setAttribute('viewbox', '0 0 570px 570px');
+                                        svg.setAttribute('viewbox', '0 0 570 570');
                                         console.log('1.0.1', svg, svg.lastElementChild);
                                         var x = (svg.clientWidth - svg.lastElementChild.clientWidth) / 2;
                                         var y = (svg.clientHeight - svg.lastElementChild.clientWidth) / 2;
